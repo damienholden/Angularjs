794bd00b61b1c861f2af46063df920c278995de1
Damien Holden <damien.holden@mycit.ie>
2017-04-05 21:09:06 +0100

    [This is a test push]

---
+++ app/app.js
@@ -0,0 +1,65 @@
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute','ngAnimate']);
+
+myNinjaApp.config(['$routeProvider', function($routeProvider){
+$routeProvider
+  .when('/home', {
+  templateUrl: 'views/home.html',
+  controller: 'NinjaController'
+})
+  .when('/directory',{
+  templateUrl: 'views/directory.html',
+  controller: 'NinjaController'
+}).otherwise({
+  redirectTo: '/home'
+  });
+
+}]);
+
+myNinjaApp.directive('randomNinja', [function (){
+
+  return {
+    restrict:'E',
+    scope: {
+      ninjas: '=',
+      title: '='
+    },
+    templateUrl: 'views/random.html',
+    transclude: true,
+    replace: true,
+    controller: function($scope){
+      $scope.random = Math.floor(Math.random()*3);
+    }
+  };
+}]);
+
+myNinjaApp.controller('NinjaController', ['$scope','$http', function($scope, $http){
+
+//$scope.message="Hey Y'all";
+$scope.removeNinja = function(ninja){
+  var removedNinja = $scope.ninjas.indexOf();
+  $scope.ninjas.splice(removedNinja,1);
+};
+  $scope.addNinja = function(){
+    $scope.ninjas.push(
+      {
+        name: $scope.newninja.name,
+        belt: $scope.newninja.belt,
+        rate: parseInt($scope.newninja.rate),
+        available: true
+      });
+      $scope.newninja.name = "";
+      $scope.newninja.belt = "";
+      $scope.newninja.rate = "";
+
+  };
+
+  $scope.removeAll = function(){
+  $scope.ninjas = [];    
+  };
+
+  $http.get('data/ninjas.json').then(function(response){
+
+    $scope.ninjas = response.data;
+  });﻿
+
+}]);
